916af1c5f3a93435fced67ac466539072d724130
Uncle_drew <47592224+Drew233@users.noreply.github.com>
2020-04-11 09:26:41 +0800

    Update shuo.js

--- src/shuo.js
+++ src/shuo.js
@@ -9,7 +9,6 @@ document.write("<script type=\"text/javascript\" src=\"https://cdn.jsdelivr.net/
     function savecontent(){
         var shuoshuo=  document.getElementById("neirong").value;
         var key = document.getElementById("key").value;
-        alert(shuoshuo);
         hash = hex_md5(key);
         for(var i=0;i<100;i++){
             hash=hex_md5(hash);
@@ -27,7 +26,6 @@ document.write("<script type=\"text/javascript\" src=\"https://cdn.jsdelivr.net/
         testObject.set('content', shuoshuo);
         testObject.save().then(function (testObject) {
         })
-        alert("上传成功");
         location.reload();
     }
     function seecontent(){
@@ -45,7 +43,7 @@ document.write("<script type=\"text/javascript\" src=\"https://cdn.jsdelivr.net/
             const resDate = d.getFullYear() + '-' + this.p((d.getMonth() + 1)) + '-' + this.p(d.getDate())
             const resTime = this.p(d.getHours()) + ':' + this.p(d.getMinutes()) + ':' + this.p(d.getSeconds())
             var li=document.createElement('li');
-            var cc="<span class=\"shuoshuo_author_img\"><img src=\""+img+"\"class=\"avatar avatar-48\" width=\"48\" height=\"48\"></span><a class=\"cbp_tmlabel\" ><p></p><p>"+uncle+"</p><p></p><p class=\"shuoshuo_time\"><i class=\"fa fa-clock-o\"></i>"+ resDate+" "+resTime+"</p></a>"
+            var cc="<span class=\"shuoshuo_author_img\"><img src=\""+img+"\"class=\"avatar avatar-48\" width=\"48\" height=\"48\"></span><a class=\"cbp_tmlabel\" ><p></p><p>"+uncle+"</p><p></p><p class=\"shuoshuo_time\"><i class=\"fa fa-clock-o\"></i>"+" "+ resDate+" "+resTime+"</p></a>"
                 li.innerHTML=cc;
                 var ul=document.getElementById("maina");
                 ul.appendChild(li);
