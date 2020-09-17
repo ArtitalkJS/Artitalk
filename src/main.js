42cb8fce45f1252a60a364e8727f55631f88bd4f
Uncle_drew <47592224+Drew233@users.noreply.github.com>
2020-09-17 16:52:18 +0800

    Dev (#56)
    * pre 3.1.0
    
    * 3.1.0
    
    * comment count
    
    * comment on or off
    
    * 3.1.2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 'use strict'
-const atVersion = "3.1.1";
+const atVersion = "3.1.2";
 if(typeof(appID) !== 'undefined'){
     let update = confirm("　　3.0版本更新需要修改一下配置，深感抱歉，但是为了更好的体验不得不这样。\n　　如果不想更换，可以继续使用2.4.2版本，这也是比较成熟的版本了，但是以后不会再维护。更换详情请点击确定前往文档查看");
     if(update == true){
@@ -1323,6 +1323,7 @@ atEvery.prototype.seeContent=function(pageNum,option){
                 fadeOut('commentNick');
                 fadeOut('email');
             } 
+            fadeIn('email');
             fadeOut('lazy');
         })
         let saveComment = document.getElementById('commentSave');
